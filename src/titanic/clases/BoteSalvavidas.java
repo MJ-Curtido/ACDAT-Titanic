cc0ef221039cae020e6da33db322cb2aa567e7a6
MJ-Curtido <manu17pc@gmail.com>
2022-09-20 12:46:33 +0200

    V5

--- src/titanic/clases/BoteSalvavidas.java
+++ src/titanic/clases/BoteSalvavidas.java
@@ -1,7 +1,44 @@
 package titanic.clases;
 
+import titanic.enums.TipoZona;
+
 public class BoteSalvavidas {
-	private static Integer id;
-	private Integer numPlaza;
+	private static Integer id = 0;
+	private Integer idBote, numPlaza;
+	private TipoZona zona;
 	
+	public BoteSalvavidas(Integer numPlaza, TipoZona zona) {
+		this.numPlaza = numPlaza;
+		this.zona = zona;
+		this.idBote = ++id;
+	}
+
+	public static Integer getId() {
+		return id;
+	}
+
+	public static void setId(Integer id) {
+		BoteSalvavidas.id = id;
+	}
+
+	public Integer getNumPlaza() {
+		return numPlaza;
+	}
+
+	public void setNumPlaza(Integer numPlaza) {
+		this.numPlaza = numPlaza;
+	}
+
+	public TipoZona getZona() {
+		return zona;
+	}
+
+	public void setZona(TipoZona zona) {
+		this.zona = zona;
+	}
+
+	@Override
+	public String toString() {
+		return "id=" + id + ", numero de plazas=" + numPlaza + ", zona=" + zona;
+	}
 }
--- src/titanic/enums/TipoZona.java
+++ src/titanic/enums/TipoZona.java
@@ -1,5 +1,21 @@
 package titanic.enums;
 
+import java.util.List;
+
 public enum TipoZona {
-	PROA, POPA, BABOR, ESTRIBOR;
+	PROA(List.of(1, 2, 3)), POPA(List.of(4, 5, 6)), BABOR(List.of(7, 8, 9)), ESTRIBOR(List.of(10, 11, 12));
+	
+	private List<Integer> escaleras;
+	
+	private TipoZona(List<Integer> escaleras) {
+		this.escaleras = escaleras;
+	}
+	
+	public void setEscaleras(List<Integer> escaleras) {
+		this.escaleras = escaleras;
+	}
+	
+	public List<Integer> getEscaleras() {
+		return this.escaleras;
+	}
 }
